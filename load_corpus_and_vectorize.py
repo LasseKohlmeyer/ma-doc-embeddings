82b13ceefdfc40da965048f424d7a1e9cdfdb2cb
Lasse Kohlmeyer <l.kohlmeyer@gmx.net>
2021-01-12 16:48:53 +0100

    experiments

--- load_corpus_and_vectorize.py
+++ load_corpus_and_vectorize.py
@@ -14,24 +14,41 @@ import timeit
 from vectorization import Vectorizer
 
 if __name__ == "__main__":
+    # corpus = DataHandler.load_maharjan_goodreads()
+    # Preprocesser.annotate_and_save(corpus, corpus_dir="corpora/goodreads_genres")
+    data_set_name = "goodreads_genres"
+    corpus = Corpus.fast_load(path=os.path.join('corpora', data_set_name), load_entities=False)
+    for doc_id, document in corpus.documents.items():
+        print(document.genres)
     # corpus = DataHandler.load_classic_gutenberg_as_corpus()
     # Preprocesser.annotate_and_save(corpus, corpus_dir="corpora/classic_gutenberg")
-    data_set_name = "classic_gutenberg"  # "german_series"  #
-    corpus = Corpus.fast_load(path=os.path.join('corpora', data_set_name), load_entities=False)
+    # data_set_name = "classic_gutenberg"  # "german_series"  #
+    # corpus = Corpus.fast_load(path=os.path.join('corpora', data_set_name), load_entities=False)
+
+    # for doc_id, document in corpus.documents.items():
+    #     # print(doc_id)
+    #     chunks = document.into_chunks(500)
+    #     for chunk in chunks:
+    #         continue
+    #         # print(chunk.length, chunk, chunk.sentences)
+
+
+
+
 
     # for doc_id, doc in corpus.documents.items():
     #     doc.load_sentences_from_disk()
     #     print(doc_id, doc.length)
-    vectorization_algorithm = "book2vec"
-    vec_file_name = Vectorizer.build_vec_file_name('long',
-                                                   '',
-                                                   data_set_name,
-                                                   'no_filter',
-                                                   vectorization_algorithm,
-                                                   'real')
-    if not os.path.isfile(vec_file_name):
-        Vectorizer.algorithm(input_str=vectorization_algorithm,
-                             corpus=corpus,
-                             save_path=vec_file_name,
-                             return_vecs=False,
-                             chunk_len=10000)
+    # vectorization_algorithm = "book2vec"
+    # vec_file_name = Vectorizer.build_vec_file_name('long_chunk',
+    #                                                '',
+    #                                                data_set_name,
+    #                                                'no_filter',
+    #                                                vectorization_algorithm,
+    #                                                'real')
+    # if not os.path.isfile(vec_file_name):
+    #     Vectorizer.algorithm(input_str=vectorization_algorithm,
+    #                          corpus=corpus,
+    #                          save_path=vec_file_name,
+    #                          return_vecs=False,
+    #                          chunk_len=10000)
